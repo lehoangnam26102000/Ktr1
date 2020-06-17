a0088d3f06b055a837c5a98883414df684c96204
lehoangnam26102000 <lehoangnam26102000@gmail.com>
2020-06-17 22:55:32 +0700

    LTW-Ktr1

---
+++ BaiKtr1/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace BaiKtr1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.txtKQ = new System.Windows.Forms.RichTextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.txtC = new System.Windows.Forms.TextBox();
+            this.btnTinhtoan = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(59, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hệ số a";
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(192, 33);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(100, 22);
+            this.txtA.TabIndex = 1;
+            this.txtA.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtA_KeyPress);
+            // 
+            // txtKQ
+            // 
+            this.txtKQ.Cursor = System.Windows.Forms.Cursors.No;
+            this.txtKQ.Enabled = false;
+            this.txtKQ.Location = new System.Drawing.Point(192, 241);
+            this.txtKQ.Name = "txtKQ";
+            this.txtKQ.ReadOnly = true;
+            this.txtKQ.Size = new System.Drawing.Size(329, 96);
+            this.txtKQ.TabIndex = 2;
+            this.txtKQ.Text = "";
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(192, 89);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(100, 22);
+            this.txtB.TabIndex = 3;
+            // 
+            // txtC
+            // 
+            this.txtC.Location = new System.Drawing.Point(192, 149);
+            this.txtC.Name = "txtC";
+            this.txtC.Size = new System.Drawing.Size(100, 22);
+            this.txtC.TabIndex = 4;
+            this.txtC.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtC_KeyDown);
+            // 
+            // btnTinhtoan
+            // 
+            this.btnTinhtoan.Location = new System.Drawing.Point(422, 68);
+            this.btnTinhtoan.Name = "btnTinhtoan";
+            this.btnTinhtoan.Size = new System.Drawing.Size(99, 23);
+            this.btnTinhtoan.TabIndex = 5;
+            this.btnTinhtoan.Text = "Tính toán";
+            this.btnTinhtoan.UseVisualStyleBackColor = true;
+            this.btnTinhtoan.Click += new System.EventHandler(this.btnTinhtoan_Click);
+            //this.btnTinhtoan.KeyDown += new System.Windows.Forms.KeyEventHandler(this.btnTinhtoan_KeyDown);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(422, 130);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(75, 23);
+            this.btnThoat.TabIndex = 6;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(59, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Hệ số b";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(59, 154);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Hệ số c";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnTinhtoan);
+            this.Controls.Add(this.txtC);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.txtKQ);
+            this.Controls.Add(this.txtA);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.RichTextBox txtKQ;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.TextBox txtC;
+        private System.Windows.Forms.Button btnTinhtoan;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ BaiKtr1/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BaiKtr1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTinhtoan_Click(object sender, EventArgs e)
+        {
+            double x1,x2;
+            double delta;
+            int a = int.Parse(txtA.Text);
+            int b = int.Parse(txtB.Text);
+            int c = int.Parse(txtC.Text);
+            delta = (b * b) - (4 * a * c);
+            if(a<=0)
+            {
+                txtKQ.Text = "Phương trình vô nghiệm";
+            }
+            if(a>0)
+            {
+                if(delta<0)
+                {
+                    txtKQ.Text = "Phương trình vô nghiệm";
+                }
+
+                if(delta==0)
+                {
+                    x1 = x2 = (-b) / (2 * a);
+                    txtKQ.Text = "Phương trình có nghiệm kép: "+"x1=x2= "+x1;
+                }
+
+                if(delta>0)
+                {
+                    x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+                    x2= (-b - Math.Sqrt(delta)) / (2 * a);
+                    txtKQ.Text = "Phương trình có 2 nghiệm: " + "x1= " + x1 + ", " + "x2= " + x2;
+                }
+            }
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            DialogResult a = MessageBox.Show("Bạn muốn thoát form", "", MessageBoxButtons.YesNo, MessageBoxIcon.Error);
+            if(a==DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void txtA_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar) && !Char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+                MessageBox.Show("Bạn chỉ được nhập số");
+            }
+        }
+
+        private void txtB_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar) && !Char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+                MessageBox.Show("Bạn chỉ được nhập số");
+            }
+        }
+
+        private void txtC_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar) && !Char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+                MessageBox.Show("Bạn chỉ được nhập số");
+            }
+        }
+
+        private void txtC_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode==Keys.Enter)
+            {
+                btnTinhtoan.PerformClick();
+            }
+            if (e.KeyCode == Keys.Escape)
+            {
+                btnThoat.PerformClick();
+            }
+        }
+    }
+}
